9be9539bf543125842d1e4d0407691e6aa4054e4
Daniel Friesen <daniel@nadir-seen-fire.com>
2017-05-03 11:27:03 -0700

    Partially implement primary/accent/dark/light handling for icons

--- src/Icon.js
+++ src/Icon.js
@@ -1,8 +1,10 @@
 'use strict';
 import React, {PureComponent} from 'react';
+import {StyleSheet} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import withMaterialTheme from './styles/withMaterialTheme';
+import shades from './styles/shades';
 
 export function createIconComponent(VectorIcon, displayName='Icon') {
 	class Icon extends PureComponent {
@@ -12,13 +14,45 @@ export function createIconComponent(VectorIcon, displayName='Icon') {
 		};
 
 		render() {
-			const {materialTheme, inactive, size, color, ...props} = this.props;
-			const themeColor = inactive ? materialTheme.icon.inactiveColor : materialTheme.icon.activeColor;
+			const {
+				materialTheme,
+				inactive,
+				size,
+				primary,
+				accent,
+				dark,
+				light,
+				color: explicitColor,
+				style,
+				...props
+			} = this.props;
+
+			let color, opacity, fadeStyle;
+			if ( explicitColor ) {
+				color = explicitColor; // @todo Handle inactive/should this fade?
+			} else if ( primary ) {
+				color = this.props.materialTheme.palette.primary;
+				opacity = inactive ? shades.opacity.dark.inactiveIcon : shades.opacity.dark.activeIcon; // @fixme autodetect dark/light
+			} else if ( accent ) {
+				color = this.props.materialTheme.palette.accent;
+				opacity = inactive ? shades.opacity.dark.inactiveIcon : shades.opacity.dark.activeIcon; // @fixme autodetect dark/light
+			} else if ( dark ) {
+				color = inactive ? shades.dark.inactiveIcon : shades.dark.activeIcon;
+			} else if ( light ) {
+				color = inactive ? shades.light.inactiveIcon : shades.light.activeIcon;
+			} else {
+				color = inactive ? materialTheme.icon.inactiveColor : materialTheme.icon.activeColor;
+			}
+
+			if ( opacity ) {
+				fadeStyle = {opacity};
+			}
 
 			return (
 				<VectorIcon
 					{...props}
-					color={color || themeColor}
+					style={[fadeStyle, style]}
+					color={color}
 					size={size} />
 			);
 		}
